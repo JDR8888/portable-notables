6fdbb0d40f10aa33da9bd497a12f44a7b644ad21
jake rudlong <jrudlong@gmail.com>
2023-04-08 01:00:13 -0500

    adds logic to src-sw and install.js files

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,23 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+	window.deferredPrompt = event;
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const deferPrompt = window.deferredPrompt;
+
+    if(!deferPrompt) {
+        return
+    }
+    // clear event after
+    window.deferredPrompt = null;
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('wow you installed an app ', event);
+});
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,10 +1,11 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { InjectManifest, generateSW } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
+const path = require('path');
 
 module.exports = () => {
   return {
@@ -18,12 +19,74 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new MiniCssExtractPlugin(),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js'
+      }),
+      new GenerateSW({
+				// excludes images from being cached initially
+				exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+
+				// runtime caching for images
+				runtimeCaching: [
+					{
+						urlPattern: /.(?:png|jpg|jpeg|svg)$/,
+						handler: "CacheFirst",
+
+						options: {
+							cacheName: "images",
+							expiration: { maxEntries: 10 },
+						},
+					},
+				],
+			}),
+      new WebpackPwaManifest({
+				// sets name and short name for app
+				name: "Text Editor",
+				short_name: "JATE",
+				// sets background color for app
+				background_color: "#FFFFFF",
+				// sets display as standalone and orientation portrait
+				display: "standalone",
+				orientation: "portrait",
+				// disables fingerprinting
+				fingerprints: false,
+				// set publicPath
+				publicPath: "./",
+				// set icon src and destination (and sizes that are saved)
+				icons: {
+					src: path.resolve("src/images/logo.png"),
+					sizes: [96, 120, 152, 167, 180, 1024],
+					destination: path.join("assets", "icons"),
+				},
+			}),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
